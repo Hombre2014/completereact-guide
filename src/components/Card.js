e8276fec6f940d5acf94dcb29fc1a7185935c7c8
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-01-18 13:02:57 +0100

    Section 3 finished - components from Expenses project

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,13 @@
 import './Card.css';
 
-const Card = (props) => {
+function Card(props) {
   const classes = 'card ' + props.className;
+
   return (
     <div className={classes}>
       {props.children}
     </div>
-  )
+  );
 }
 
 export default Card;
--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,8 +1,6 @@
-import React from 'react';
-
 import './ExpenseDate.css';
 
-const ExpenseDate = (props) => {
+function ExpenseDate(props) {
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date.getFullYear();
@@ -13,7 +11,7 @@ const ExpenseDate = (props) => {
       <div className='expense-date__year'>{year}</div>
       <div className='expense-date__day'>{day}</div>
     </div>
-  );
-};
+  )
+}
 
 export default ExpenseDate;
